8cba4dbd7b629f61d0d9b4c8bafc5efdb1964aba
Benjamin Hipple <brh2113@columbia.edu>
2015-03-07 13:59:45 -0500

    Updated loader to dict

--- programming_6/test_sum.py
+++ programming_6/test_sum.py
@@ -2,27 +2,36 @@
 from two_sums import *
 import unittest
 
-def loadSet(filePath):
+def loadDict(filePath):
     fh = open(filePath)
-    nums = set()
+    nums = {}
     for line in fh:
-        nums.add(int(line))
+        num = int(line)
+        if num in nums:
+            nums[num] += 1
+        else:
+            nums[num] = 1
     return nums
 
 class Tester(unittest.TestCase):
+    def test_load(self):
+        d = loadDict('82.txt')
+        self.assertEqual(1, d[1])
+        self.assertEqual(2, d[4])
+        self.assertEqual(1, d[-4])
+
     def test_54(self):
-        self.assertEqual(54, twoSums(loadSet('54.txt')))
+        self.assertEqual(54, twoSums(loadDict('54.txt')))
 
     def test_3(self):
-        self.assertEqual(3, twoSums(loadSet('3.txt')))
+        self.assertEqual(3, twoSums(loadDict('3.txt')))
 
     def test_82(self):
-        self.assertEqual(82, twoSums(loadSet('82.txt')))
+        self.assertEqual(82, twoSums(loadDict('82.txt')))
 
-    def test_hw(self):
-        if False:
-            nums = loadSet('algo1_programming_prob_2sum.txt')
-            print twoSums(nums)
+    def disable(self):
+        nums = loadDict('algo1_programming_prob_2sum.txt')
+        print twoSums(nums)
 
 if __name__ == '__main__':
     unittest.main()
--- programming_6/two_sums.py
+++ programming_6/two_sums.py
@@ -3,4 +3,5 @@
 # compute the number of target values t in the interval [-10000,10000] (inclusive)
 # such that there are distinct numbers x,y in the input file that satisfy x+y=t.
 def twoSums(nums):
-    return 0
+    res = 0
+    return res
