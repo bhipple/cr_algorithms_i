bbff2737047c006a29bc39bfa5891966af245f2c
Benjamin Hipple <brh2113@columbia.edu>
2015-03-07 14:22:32 -0500

    Final version of test harness with more test cases

--- programming_6/test_sum.py
+++ programming_6/test_sum.py
@@ -2,35 +2,29 @@
 from two_sums import *
 import unittest
 
-def loadDict(filePath):
-    fh = open(filePath)
-    nums = {}
-    for line in fh:
-        num = int(line)
-        if num in nums:
-            nums[num] += 1
-        else:
-            nums[num] = 1
+def loadSet(filePath):
+    nums = set()
+    map(lambda x: nums.add(int(x)), open(filePath).readlines())
     return nums
 
 class Tester(unittest.TestCase):
-    def test_load(self):
-        d = loadDict('82.txt')
-        self.assertEqual(1, d[1])
-        self.assertEqual(2, d[4])
-        self.assertEqual(1, d[-4])
+    def test_distinct(self):
+        self.assertEqual(0, twoSums(loadSet('0.txt')))
 
-    def test_54(self):
-        self.assertEqual(54, twoSums(loadDict('54.txt')))
+    def test_2(self):
+        self.assertEqual(1, twoSums(loadSet('1.txt')))
 
     def test_3(self):
-        self.assertEqual(3, twoSums(loadDict('3.txt')))
+        self.assertEqual(3, twoSums(loadSet('3.txt')))
 
-    def test_82(self):
-        self.assertEqual(82, twoSums(loadDict('82.txt')))
+    def test_5(self):
+        self.assertEqual(5, twoSums(loadSet('5.txt')))
+
+    def test_6(self):
+        self.assertEqual(6, twoSums(loadSet('6.txt')))
 
     def disable(self):
-        nums = loadDict('algo1_programming_prob_2sum.txt')
+        nums = loadSet('algo1_programming_prob_2sum.txt')
         print twoSums(nums)
 
 if __name__ == '__main__':
